8864a3da087d404f47c6fc9b7f8ae173f3f35e53
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2020-04-20 22:02:28 +0100

    configured Hystrix command with commandProperties

--- bookshop_sales/src/main/java/com/emeka/bookshop_sales/services/BookService.java
+++ bookshop_sales/src/main/java/com/emeka/bookshop_sales/services/BookService.java
@@ -2,6 +2,7 @@ package com.emeka.bookshop_sales.services;
 
 import com.emeka.bookshop_sales.OrderRequest;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,15 @@ public class BookService {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackBookName")
+    @HystrixCommand(
+            fallbackMethod = "getFallbackBookName",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value="6"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value="50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value="6000")
+            }
+    )
     public String getBookName(@RequestBody OrderRequest orderRequest) {
         return restTemplate.getForObject("http://bookshop-inventory-service/books/" + orderRequest.getBookId(), String.class);
     }
--- bookshop_sales/src/main/java/com/emeka/bookshop_sales/services/StaffService.java
+++ bookshop_sales/src/main/java/com/emeka/bookshop_sales/services/StaffService.java
@@ -2,6 +2,7 @@ package com.emeka.bookshop_sales.services;
 
 import com.emeka.bookshop_sales.OrderRequest;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,15 @@ public class StaffService {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackStaffName")
+    @HystrixCommand(
+            fallbackMethod = "getFallbackStaffName",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value="6"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value="50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value="6000")
+            }
+    )
     public String getStaffName(@RequestBody OrderRequest orderRequest) {
         return restTemplate.getForObject("http://staff-management-service/staff/" + orderRequest.getStaffId(), String.class);
     }
